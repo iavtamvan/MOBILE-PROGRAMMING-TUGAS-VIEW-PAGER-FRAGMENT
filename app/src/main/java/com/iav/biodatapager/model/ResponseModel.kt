59ee93f60d291257466bc12df38a1a51a450f5d2
iavtamvan <ade.fajr.ariav@gmail.com>
2019-03-20 12:52:04 +0700

    fixed center tab, rxjava, retrofit, rxkotlin

--- app/src/main/java/com/iav/biodatapager/model/ResponseModel.kt
+++ app/src/main/java/com/iav/biodatapager/model/ResponseModel.kt
@@ -1,3 +1,9 @@
 package com.iav.biodatapager.model
 
-data class ResponseModel(val events: List<Item>, val teams: List<Item>, val player: List<Item>, val players: List<Item>, val event: List<Item>)
+data class ResponseModel(
+    val events: List<Item>,
+    val teams: List<Item>,
+    val player: List<Item>,
+    val players: List<Item>,
+    val event: List<Item>
+)
--- app/src/main/java/com/iav/biodatapager/rest/ApiService.kt
+++ app/src/main/java/com/iav/biodatapager/rest/ApiService.kt
@@ -9,6 +9,7 @@ interface ApiService {
 
     @GET("eventsnextleague.php")
     fun getNextMatch(@Query("id") id: String): Observable<ResponseModel>
+
     @GET("lookup_all_teams.php")
     fun getTeamByLigaId(@Query("id") id: String): Observable<ResponseModel>
 }
